87ac392d18267d5d4f55f775e7dd9c7b5c62fc6a
bennett33 <blanzetta3@gmail.com>
2021-12-20 23:08:16 -0800

    initial commit

---
+++ pizza.js
@@ -0,0 +1,58 @@
+// pizza 1
+function pizzaOven(crustType, sauceType, cheese, toppings) {
+    var pizza = {};
+    pizza.crustType = crustType;
+    pizza.sauceType = sauceType;
+    pizza.cheese = cheese;
+    pizza.toppings = toppings;
+    return pizza;
+}
+
+var p1 = pizzaOven("deep dish", "traditional", ["mozzarella"], ["pepperoni", "sausage"]);
+console.log(p1);
+
+
+// pizza 2
+function pizzaOven(crustType, sauceType, cheese, toppings) {
+    var pizza = {};
+    pizza.crustType = crustType;
+    pizza.sauceType = sauceType;
+    pizza.cheese = cheese;
+    pizza.toppings = toppings;
+    return pizza;
+}
+
+var p2 = pizzaOven("hand tossed", "marinara", ["mozzarella", "feta"], ["mushrooms", "olives", "onions"]);
+console.log(p2);
+
+
+
+// pizza 3
+function pizzaOven(crustType, sauceType, cheese, toppings) {
+    var pizza = {};
+    pizza.crustType = crustType;
+    pizza.sauceType = sauceType;
+    pizza.cheese = cheese;
+    pizza.toppings = toppings;
+    return pizza;
+}
+
+var p3 = pizzaOven("deep dish", "pesto", ["ricotta"], ["pineapple", "chicken"]);
+console.log(p3);
+
+
+
+// pizza 4
+function pizzaOven(crustType, sauceType, cheese, toppings) {
+    var pizza = {};
+    pizza.crustType = crustType;
+    pizza.sauceType = sauceType;
+    pizza.cheese = cheese;
+    pizza.toppings = toppings;
+    return pizza;
+}
+
+var p4 = pizzaOven("thin crust", "olive oil", ["mozzarella"], ["garlic", "sausage"]);
+console.log(p4);
+
+
